76d11fba14e276e6c70bc534f2162a21a73a4aeb
aarav010710 <76567151+aarav010710@users.noreply.github.com>
2021-10-05 17:37:56 +0530

    Add files via upload

---
+++ garage.js
@@ -0,0 +1,61 @@
+function preload()
+{
+    img = loadImage('garage.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+}
+
+function draw()
+{
+   image(img, 0,0, 380, 380);
+
+ 
+   if (status_detection != "")
+   {
+       for(i = 0; i < objects.length; i++)
+       {
+       document.getElementById("status").innerHTML = "Status: Objects Detected";
+       fill("#FF0000");
+       percentage = floor(objects[i].confidence *100);
+       text(objects[i].label + " " + percentage + "%", objects[i].x+15 , objects[i].y+15);
+       noFill();
+       stroke("#FFOOOO");
+       rect(objects[i].x , objects[i].y, objects[i].width, objects[i].height);
+   }
+}
+}
+
+function backg()
+{
+    window.location = "home.html";
+}
+
+img = "";
+status_detection = "";
+objects = [];
+
+function modelloaded()
+{
+    console.log("CocoSsd is loaded!!");
+    status_detection = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error , results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
+
+}
---
+++ home.js
@@ -0,0 +1,19 @@
+function totv()
+{
+window.location = "tv.html";
+}
+
+function elecimg()
+{
+    window.location = "elec.html";
+}
+
+function garageimg()
+{
+    window.location = "garage.html";
+}
+
+function bedimg()
+{
+    window.location = "bedroom.html";
+}
